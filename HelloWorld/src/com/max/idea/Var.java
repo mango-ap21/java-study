3fa06b3cad6afa3649468d186a55c0492737ba5a
IBS\APronina <APronina@IBS.RU>
2023-08-15 13:39:14 +0300

    Задача №2

---
+++ HelloWorld/src/com/max/idea/Var.java
@@ -0,0 +1,14 @@
+package com.max.idea;
+
+public class Var {
+    public static void main(String[] args) {
+//        Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел.
+//        Присвойте ей произвольное дробное значение
+        double doubleVar = 20.23;
+//        Создайте переменную целочисленного типа с наименьшим диапазоном чисел.
+//        Присвойте ей наибольшее возможное значение
+        byte byteVar = 127;
+
+        System.out.println("doubleVar: " + doubleVar + "\nbyteVar: " + byteVar);
+    }
+}
